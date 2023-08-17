19f3cf1caeee90a600425fc4aaf0f12354c52420
euripus <epistomoclus@gmail.com>
2023-08-17 06:14:22 +0300

    x

--- src/gui/nineslice.h
+++ src/gui/nineslice.h
@@ -2,27 +2,7 @@
 #define NINESLICE_H
 
 #include "src/VertexBuffer.h"
-#include <glm/glm.hpp>
-
-//                      (right_top, tx1)
-//  --------------------
-//  |                  |
-//  |                  |
-//  |                  |
-//  |                  |
-//  --------------------
-// (left_bottom, tx0)
-struct RegionDataOfUITexture
-{
-    glm::vec2 left_bottom;   // pixel coordinates
-    glm::vec2 right_top;
-    glm::vec2 tx0;   // normalized coordinates
-    glm::vec2 tx1;
-
-    float     getWidth() const { return right_top.x - left_bottom.x; }
-    float     getHeight() const { return right_top.y - left_bottom.y; }
-    glm::vec2 getSize() const { return {getWidth(), getHeight()}; }
-};
+#include "src/gui/uiimagemanager.h"
 
 struct NineSliceBlock
 {
---
+++ src/gui/uiimagemanager.h
@@ -0,0 +1,38 @@
+#ifndef UIIMAGEMANAGER_H
+#define UIIMAGEMANAGER_H
+
+#include <vector>
+#include <glm/glm.hpp>
+
+#include "src/gui/imagedata.h"
+
+//                      (right_top, tx1)
+//  --------------------
+//  |                  |
+//  |                  |
+//  |                  |
+//  |                  |
+//  --------------------
+// (left_bottom, tx0)
+struct RegionDataOfUITexture
+{
+	std::uint32_t id;
+
+    glm::vec2 left_bottom;   // pixel coordinates
+    glm::vec2 right_top;
+    glm::vec2 tx0;   // normalized coordinates
+    glm::vec2 tx1;
+
+    float     getWidth() const { return right_top.x - left_bottom.x; }
+    float     getHeight() const { return right_top.y - left_bottom.y; }
+    glm::vec2 getSize() const { return {getWidth(), getHeight()}; }
+};
+
+class UIImageManager
+{
+private:
+	std::vector<RegionDataOfUITexture> m_regions;
+	AtlasTex m_atlas;   // one tex atlas for all loaded UI elements
+};
+
+#endif
