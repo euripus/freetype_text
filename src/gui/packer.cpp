80e218452456aeeeb9e890bad85d3797fbe4cc5c
euripus <epistomoclus@gmail.com>
2023-09-28 06:33:13 +0300

    x

--- src/gui/packer.cpp
+++ src/gui/packer.cpp
@@ -2,3 +2,48 @@
 
 void Packer::fitWidgets(Widget * root)
 {}
+
+FinalList Packer::getListFromTree(Widget * root)
+{
+	FinalList list;	
+	
+	if(root != nullptr)
+		addSubTree(list, root);
+	
+	std::reverse(std.begin(list), std::end(list));
+	
+	return list;
+}
+
+void Packer::addSubTree(FinalList & ls, Widget * root)
+{
+	ls.push_back({});
+	auto & ch_list = ls.back();
+	
+	if(root->m_state == VerticalLayoutee || root->m_state == HorizontalLayoutee)
+	{
+		for(auto const & ch : root->m_children)
+		{
+			if(auto const * cur_ch = ch.get(); cur_ch->m_state != VerticalLayoutee || cur_ch->m_state != HorizontalLayoutee)
+			{
+				ch_list.push_back(cur_ch);
+			}
+			else
+			{
+				if(cur_ch->m_state == HorizontalLayoutee)
+					addSubTree(list, cur_ch);
+				else
+				{
+					for(auto const & ch2 : cur_ch->m_children)
+					{
+						addSubTree(list, ch2.get());
+					}
+				}
+			}
+		}
+	}
+	else
+	{
+		ch_list.push_back(root);
+	}
+}
--- src/gui/packer.h
+++ src/gui/packer.h
@@ -6,10 +6,15 @@
 class Packer
 {
 public:
+	using FinalList = std::vector<std::vector<Widget *>>;
+
     void fitWidgets(Widget * root);
     void setSpacing(float val) { m_spacing = val; }
 
 protected:
+	FinalList getListFromTree(Widget * root);
+	void addSubTree(FinalList & ls, Widget * root);
+	
     float m_spacing;
 };
 
